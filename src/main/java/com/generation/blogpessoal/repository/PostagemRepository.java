12b05cf9b9bbc12fd61b3788566d89a863c995ff
Bella <bellsceccon@gmail.com>
2026-07-17 17:38:27 -0300

    :tada: Commit inicial - Blog Pessoal

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,15 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+	public List<Postagem> findAllByTituloContaingIgnoreCase(String titulo);
+	
+	// SELECT * FROM tb_postagens WHERW titulo LIKE "%?%";
 	
 }
